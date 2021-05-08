7ca97fa1d2c5d03b17726a3e8d0893ca7148b071
adrianjost <22987140+adrianjost@users.noreply.github.com>
2021-05-08 10:01:34 +0200

    🛑 stop syncing test and lint code with SmartLight-Google-Home

--- scripts/generator.js
+++ scripts/generator.js
@@ -36,7 +36,7 @@ const syncedRepos = {
 	"SmartLight-Database-Functions": defaultExluding(["CI-Lint", "CI-Build"]),
 	// "SmartLight-Documentation",
 	"SmartLight-Firmware": ["Secrets", "GitHub Automation"],
-	"SmartLight-Google-Home": [...defaultExluding(["CI-Build"]), "CI-Test"],
+	"SmartLight-Google-Home": defaultExluding([ "CI-Lint", "CI-Build"]),
 	// "SmartLight-Hardware",
 	// "SmartLight-Homepage",
 	// "SmartLight-Hub",
